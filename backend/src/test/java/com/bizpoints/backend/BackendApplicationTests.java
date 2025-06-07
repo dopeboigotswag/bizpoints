87eb7367c0a9a72427daa3adff27cddd9ff72479
“Alexis <alexis.corrieras@gmail.com>
2025-06-07 14:45:51 -0400

    add backend

---
+++ backend/src/test/java/com/bizpoints/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizpoints.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
